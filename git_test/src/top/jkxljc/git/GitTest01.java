bd0c5c67806fc29683fc582787aa042d4f609159
admin <admin@qq.com>
2021-01-01 18:25:02 +0800

    第二次提交git

--- git_test/src/top/jkxljc/git/GitTest01.java
+++ git_test/src/top/jkxljc/git/GitTest01.java
@@ -15,4 +15,7 @@ public class GitTest01 {
         System.out.println("hello git");
         System.out.println("hello git");
     }
+    public void test(){
+        System.out.println(123);
+    }
 }
