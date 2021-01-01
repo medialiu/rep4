5ed502e293e74961b49e087f5ce45c7024026714
admin <admin@qq.com>
2021-01-01 20:28:34 +0800

    Second类

---
+++ git_test/src/top/jkxljc/git/Second.java
@@ -0,0 +1,12 @@
+package top.jkxljc.git;
+
+/**
+ * @author MediaLiu
+ * @Classname Second
+ * @Description TODO
+ * @Date 2021/1/1 20:27
+ * @Version V1.0
+ */
+public class Second {
+    public final static int a=1;
+}
